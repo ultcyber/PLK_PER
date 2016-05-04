11d2558d69523daf94d3ea258c89bb173978360e
ultcyber <sedov00@gmail.com>
2016-05-04 22:33:10 +0200

    Completed the scrapers for league-wide stats

--- WebProjects/PLK_PER/webscraper.py
+++ WebProjects/PLK_PER/webscraper.py
@@ -12,6 +12,8 @@ player = PERCalculator("Mateusz", "Trybulec")
 
 # Team stats AJAX capture
 
+
+
 def teamparse(headers, sibl_end):
 	
 	headers['X-Requested-With'] = 'XMLHttpRequest'
@@ -24,7 +26,9 @@ def teamparse(headers, sibl_end):
 	
 	# Create an array for the results
 	results = []
-
+	
+	#print(results) <--- Just for testing
+	
 	# For each row, calculate the appropriate next sibling (determined by the sibl_end parameter)
 	for element in rows:
 		count = 0
@@ -36,17 +40,13 @@ def teamparse(headers, sibl_end):
 	average = reduce(lambda x, y: float(x)+float(y), results)/(len(results)+1)
 	return round(average,2)
 
-
-lfieldgoals = teamparse({ 'f':0, 's':18, 'o':'Pkt', 'wh':'g0', 'r':'0', 'nocontent':1}, 4)
-
-
-# lassists
-# ltrebounds
-# lorebounds
-# lafieldgoals
-# lafreethrows
-# lfreethrows
-# lpfouls
-# lafthrows
-# lpoints
-# lturnovers
+lpoints = teamparse({ 'f':0, 's':18, 'o':'Pkt', 'wh':'g0', 'r':'0', 'nocontent':1}, 4)
+lfieldgoals = teamparse({ 'f':0, 's':18, 'o':'Pkt', 'wh':'g0', 'r':'0', 'nocontent':1}, 6)
+lafieldgoals = teamparse({ 'f':0, 's':18, 'o':'Pkt', 'wh':'g0', 'r':'0', 'nocontent':1}, 8)
+lfreethrows = teamparse({ 'f':0, 's':18, 'o':'Pkt', 'wh':'g0', 'r':'0', 'nocontent':1}, 24)
+lafreethrows = teamparse({ 'f':0, 's':18, 'o':'Pkt', 'wh':'g0', 'r':'0', 'nocontent':1}, 26)
+lorebounds = teamparse({ 'f':0, 's':18, 'o':'Pkt', 'wh':'g0', 'r':'0', 'nocontent':1}, 30)
+ltrebounds = teamparse({ 'f':0, 's':18, 'o':'Pkt', 'wh':'g0', 'r':'0', 'nocontent':1}, 34)
+lassists = teamparse({ 'f':0, 's':18, 'o':'Pkt', 'wh':'g0', 'r':'0', 'nocontent':1}, 36)
+lpfouls = teamparse({ 'f':0, 's':18, 'o':'Pkt', 'wh':'g0', 'r':'0', 'nocontent':1}, 38)
+lturnovers = teamparse({ 'f':0, 's':18, 'o':'Pkt', 'wh':'g0', 'r':'0', 'nocontent':1}, 42)
